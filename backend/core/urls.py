1a24f7dc2b709113f938582376dacc9142b711cd
rejohn <rejohn@rejohn>
2024-09-24 11:23:56 -0400

    Add landing page to base route

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import landing_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page, name='landing_page'),
     path('api/', include('notification.urls')), # Include the notification API URLs
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
